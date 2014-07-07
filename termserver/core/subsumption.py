1d9dc706f448e1d9e93be0ab5d9569e69e792e59
Dr Nure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-07 16:04:25 +0300

    Minor improvements to the subsumption tester

--- termserver/core/subsumption.py
+++ termserver/core/subsumption.py
@@ -1,5 +1,4 @@
 # -coding=utf-8
-__author__ = 'ngurenyaga'
 """
 This module implements SNOMED subsumption testing ( using an in-memory map ).
 This map is non-persistent and may therefore be re-created on every app server start.
@@ -12,6 +11,7 @@ For the data that we get back from the query below:
     destination_id is the parent
     source_id is the subtype
 """
+__author__ = 'ngurenyaga'
 
 from django.conf import settings
 from django.db import connection
@@ -29,10 +29,11 @@ class Tester(object):
         """
         The query below relies on the immutability of SNOMED IDs
         """
-        self.CHILDEN_TO_PARENTS_MAP = {}
-        self.PARENTS_TO_CHILDREN_MAP = {}
+        self.C2P_MAP = {}  # children to parents map
+        self.P2C_MAP = {}  # Parents to children map
         self.CHILDREN_TO_PARENTS_KEY = "children_to_parents_map"
         self.PARENTS_TO_CHILDREN_KEY = "parents_to_children_map"
+        # Needed so as to avoid double initialization
         self.snapshot_loaded = False
         self.setup_done = False
 
@@ -74,10 +75,9 @@ class Tester(object):
                     'Not loading SNOMED. sys.argv is "%s"' % str(sys.argv))
 
             # The actual load
-            if not len(self.CHILDEN_TO_PARENTS_MAP) and not len(self.PARENTS_TO_CHILDREN_MAP):
+            if not len(self.C2P_MAP) and not len(self.P2C_MAP):
                 # Will be lazily initialized
-                self.CHILDREN_TO_PARENTS_MAP, self.PARENTS_TO_CHILDREN_MAP = self.get_transitive_closure_map(
-                )
+                self.CHILDREN_TO_PARENTS_MAP, self.P2C_MAP = self.get_transitive_closure_map()
                 # Will be used to cache trees that have been "seen" and
                 # computed
                 self.ALL_PARENTS_CACHE = {}
@@ -87,33 +87,38 @@ class Tester(object):
             self.setup_done = True
 
     def get_transitive_closure_map(self):
+        """Generate the in-memory maps that will be used to test for subsumption"""
         cur = connection.cursor()
         cur.execute(self.IS_A_RELATIONSHIPS_QUERY)
         for (source_id, destination_id) in cur.fetchall():
             # Populate the children to parents map
-            if source_id in self.CHILDEN_TO_PARENTS_MAP:
-                if destination_id not in self.CHILDEN_TO_PARENTS_MAP[source_id]:
-                    self.CHILDEN_TO_PARENTS_MAP[
+            if source_id in self.C2P_MAP:
+                if destination_id not in self.C2P_MAP[source_id]:
+                    self.C2P_MAP[
                         source_id].append(destination_id)
             else:
-                self.CHILDEN_TO_PARENTS_MAP[source_id] = [destination_id]
+                self.C2P_MAP[source_id] = [destination_id]
 
             # Populate the parents to children map
-            if destination_id in self.PARENTS_TO_CHILDREN_MAP:
-                if source_id not in self.PARENTS_TO_CHILDREN_MAP[destination_id]:
-                    self.PARENTS_TO_CHILDREN_MAP[
+            if destination_id in self.P2C_MAP:
+                if source_id not in self.P2C_MAP[destination_id]:
+                    self.P2C_MAP[
                         destination_id].append(source_id)
             else:
-                self.PARENTS_TO_CHILDREN_MAP[destination_id] = [source_id]
+                self.P2C_MAP[destination_id] = [source_id]
 
-        return self.CHILDEN_TO_PARENTS_MAP, self.PARENTS_TO_CHILDREN_MAP
+        return self.C2P_MAP, self.P2C_MAP
 
     def get_children_of(self, parent_id):
+        """Return the children and descendants of a concept
+
+        :param parent_id:
+        """
         self._do_setup()
         output = []
 
         def _walk(current_child_id):
-            children = self.PARENTS_TO_CHILDREN_MAP.get(current_child_id, [])
+            children = self.P2C_MAP.get(current_child_id, [])
             output.extend(children)
             for next_parent_id in children:
                 _walk(next_parent_id)
@@ -122,15 +127,23 @@ class Tester(object):
         return set(_walk(parent_id))
 
     def get_direct_children_of(self, parent_id):
+        """Return the immediate children of a concept
+
+        :param parent_id:
+        """
         self._do_setup()
-        return self.PARENTS_TO_CHILDREN_MAP.get(parent_id, [])
+        return self.P2C_MAP.get(parent_id, [])
 
     def get_parents_of(self, child_id):
+        """Return the parents and ancestors of a concept
+
+        :param child_id:
+        """
         self._do_setup()
         output = []
 
         def _walk(current_child_id):
-            parents = self.CHILDEN_TO_PARENTS_MAP.get(current_child_id, [])
+            parents = self.C2P_MAP.get(current_child_id, [])
             output.extend(parents)
             for next_child_id in parents:
                 _walk(next_child_id)
@@ -139,14 +152,21 @@ class Tester(object):
         return _walk(child_id)
 
     def get_direct_parents_of(self, child_id):
+        """Return the immediate parents of a concept
+
+        :param child_id:
+        """
         self._do_setup()
-        return self.CHILDEN_TO_PARENTS_MAP.get(child_id, [])
+        return self.C2P_MAP.get(child_id, [])
 
     def is_parent_of(self, child_id, candidate_parent_id):
         """
         Return True if the SNOMED concept identified by candidate_parent_id
         is a direct or indirect parent of the one identified by child_id.
         Parents that are seen often are cached.
+
+        :param child_id:
+        :param candidate_parent_id:
         """
         self._do_setup()
         if child_id in self.ALL_PARENTS_CACHE:
@@ -160,6 +180,9 @@ class Tester(object):
         """
         Return True if the SNOMED concept identified by candidate_parent_id
         is a direct parent of the one identified by child_id
+
+        :param child_id:
+        :param candidate_parent_id:
         """
         self._do_setup()
         return candidate_parent_id in self.get_direct_parents_of(child_id)
@@ -168,6 +191,9 @@ class Tester(object):
         """
         Return True if the SNOMED concept identified by candidate_child_id
         is a direct or indirect child of the one identified by parent_id
+
+        :param parent_id:
+        :param candidate_child_id:
         """
         self._do_setup()
         if parent_id in self.ALL_CHILDREN_CACHE:
@@ -181,6 +207,9 @@ class Tester(object):
         """
         Return True if the SNOMED concept identified by candidate_child_id
         is a direct child of the one identified by parent_id
+
+        :param parent_id:
+        :param candidate_child_id:
         """
         self._do_setup()
         return candidate_child_id in self.get_direct_children_of(parent_id)
