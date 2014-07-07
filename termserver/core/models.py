50f741a0875bed4d43fcf9154149680fd3025f4e
Dr Nure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-07 14:34:06 +0300

    Implemented SNOMED Identifier validators
    Not tested yet

--- termserver/core/models.py
+++ termserver/core/models.py
@@ -1,22 +1,16 @@
 """Models for core SNOMED components ( refsets excluded )"""
 # -coding=utf-8
 from django.db import models
+from django.core.exceptions import ValidationError
+from .helpers import verhoeff_digit
 
-
-class SCTIDField(models.BigIntegerField):
-    """SNOMED ID Validation Concerns encapsulated here"""
-    description = _("SNOMED CT Identifier")
-    # TODO - Only positive integer fields
-    # TODO - Fields greater than 10^5
-    # TODO - Values less than 10^18
-    # TODO - Last digit is a Verhoeff dihedral check digit
-    # TODO - Must have 5 components; partition identifier, item identifier, namespace id, check digit
-    pass
+import math
+import re
 
 
 class Component(models.Model):
     """Fields shared between all components"""
-    sctid = SCTIDField()
+    sctid = models.BigIntegerField()
     effective_time = models.DateField()
     active = models.BooleanField()
     module = models.ForeignKey('Concept')
@@ -26,6 +20,79 @@ class Component(models.Model):
     # TODO - Different validation for different component types; use partition id
     # TODO - default export formatter for effective_time that conforms to SNOMED?
     # TODO - validator for module; that it is a descendant of the module concept
+    # TODO - Validate effective time greater than <first SNOMED release>
+
+    def _validate_sctid_minimum(self):
+        """Must be greater than 10^5"""
+        if self.sctid < math.pow(10, 5):
+            raise ValidationError("A SNOMED Identifier must be > 10^5")
+
+    def _validate_sctid_maximum(self):
+        """Must be less than 10^18"""
+        if self.sctid > math.pow(10, 18):
+            raise ValidationError("A SNOMED Identifier must be < 10^18")
+
+    def _validate_sctid_is_positive(self):
+        """Must be positive integers"""
+        if self.sctid < 0:
+            raise ValidationError("A SNOMED Identifier must be positive")
+
+    def _validate_sctid_check_digit(self):
+        """"The last digit is a Verhoeff dihedral check digit"""
+        # Split out last digit
+        s = str(self.sctid)
+        identifier, check_digit = s[:-1], self._get_sctid_check_digit()
+        if verhoeff_digit(identifier) != check_digit:
+            raise ValidationError("The SNOMED Identifier has an invalid check digit")
+
+    def _validate_sctid_no_leading_zeros(self):
+        """The string rendering of a SNOMED identifier should have no leading zeroes"""
+        pattern = re.compile('^0.+')
+        if pattern.match(str(self.sctid)):
+            raise ValidationError("The string representation should not have leading zeroes")
+
+    def _validate_identifier_components(self):
+        """A valid long format SNOMED identifier has the following components:
+            1. Item identifier
+            2. Namespace identifier
+            3. Partition identifier
+            4. Check digit
+
+        A valid short format SNOMED identifier has the following components:
+            1. Item identifier
+            2. Partition identifier
+            3. Check digit
+        """
+        if not self._sctid_is_long_format() and not self._sctid_is_short_format():
+            raise ValidationError("None of the expected partition identifiers was found")
+
+    def validate_snomed_identifier(self):
+        """Sanity checks on SNOMED identifiers"""
+        self._validate_sctid_minimum()
+        self._validate_sctid_maximum()
+        self._validate_sctid_is_positive()
+        self._validate_sctid_is_positive()
+        self._validate_sctid_check_digit()
+        self._validate_sctid_no_leading_zeros()
+        self._validate_identifier_components()
+
+    def _sctid_is_short_format(self):
+        """True if it contains 00 OR 01 OR 02 as partition identifiers"""
+        partition_identifier = self._get_sctid_partition_identifier()
+        return partition_identifier in ['00', '01', '02']
+
+    def _sctid_is_long_format(self):
+        """True if it contains 10, 11 or 12 as partition identifiers"""
+        partition_identifier = self._get_sctid_partition_identifier()
+        return partition_identifier in ['10', '11', '12']
+
+    def _get_sctid_check_digit(self):
+        """The check digit is always the last digit"""
+        return str(self.sctid)[-1]
+
+    def _get_sctid_partition_identifier(self):
+        """The partition identifier is always the last two digits"""
+        return str(self.sctid)[-4:-2]
 
     class Meta(object):
         abstract = True
@@ -44,7 +111,7 @@ class Concept(Component):
 class Description(Component):
     """SNOMED descriptions"""
     concept = models.ForeignKey(Concept)
-    language_code = models.CharField(max_length=2)
+    language_code = models.CharField(max_length=2, default='en')
     type = models.ForeignKey(Concept)
     case_significance = models.ForeignKey(Concept)
     term = models.TextField()
