6e759b2c8ae369dfac548cb1313b71835fe0f947
Dr Nure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-07 17:59:24 +0300

    Added django-extensions to third party installed app list

--- termserver/config/settings.py
+++ termserver/config/settings.py
@@ -8,12 +8,15 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 INSTALLED_APPS = (
+    # Built in apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'django_extensions',
     # Our apps
     'core',
 )
