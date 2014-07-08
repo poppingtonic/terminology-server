1eb8c9148974666a61d1ddfd34429e7eb37b79b7
Dr Nure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-08 13:24:57 +0300

    First cut implementation of simple map reference set

--- termserver/core/models.py
+++ termserver/core/models.py
@@ -368,6 +368,26 @@ class SimpleMapReferenceSet(RefsetBase):
     """Used for one to one maps between SNOMED and other code systems"""
     map_target = models.CharField(max_length=256)
 
+    def _validate_refset(self):
+        """Should be a descendant of '900000000000496009' """
+        if not SNOMED_TESTER.is_child_of(900000000000496009, self.refset.concept_id):
+            raise ValidationError("The refset must be a descendant of '900000000000496009'")
+
+    def clean(self):
+        """Perform sanity checks"""
+        self._validate_refset()
+        super(SimpleMapReferenceSet, self).clean()
+
+    def save(self, *args, **kwargs):
+        """
+        Override save to introduce validation before every save
+
+        :param args:
+        :param kwargs:
+        """
+        self.full_clean()
+        super(SimpleMapReferenceSet, self).save(*args, **kwargs)
+
     class Meta(object):
         db_table = 'snomed_simple_map_reference_set'
 
