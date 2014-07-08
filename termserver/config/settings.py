4db0a74f936494a3c023d3c90ad97bf5fd6133d1
Dr Nure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-08 15:14:27 +0300

    Added admin app - for administrative tools

--- termserver/config/settings.py
+++ termserver/config/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = (
     'refset',
     'search',
     'authoring',
-    'expression_repository'
+    'expression_repository',
+    'admin',
 )
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
