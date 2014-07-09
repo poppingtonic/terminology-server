5e38e0ba788d78426f9dad321f30dd06cb11d468
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-09 14:27:51 +0300

    Set up scaffolding for the necessary management commands

--- termserver/administration/management/commands/load_delta_drug_release.py
+++ termserver/administration/management/commands/load_delta_drug_release.py
@@ -1,1 +1,31 @@
+# coding=utf-8
+"""Load the most recent SNOMED UK drug release delta"""
 __author__ = 'ngurenyaga'
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    """Management command to load the newest delta SNOMED UK drug release"""
+    help = 'Load the newest delta ( fortnightly ) UK drug release'
+
+    def add_arguments(self, parser):
+        """Set the applicable command line arguments
+
+        :param parser:
+        """
+        # parser.add_argument('poll_id', nargs='+', type=int)
+        pass
+
+    def handle(self, *args, **options):
+        """The command's entry point"""
+        try:
+            # TODO - do the actual loading ( delegate to helpers )
+            pass
+        except:
+            # TODO - catch more specific exceptions
+            # TODO - return informative and specific error messages below
+            raise CommandError('Show a specific message here')
+
+        # TODO - write sensible feedback to standard out
+        self.stdout.write('Give user feedback here')
--- termserver/administration/management/commands/load_full_drug_release.py
+++ termserver/administration/management/commands/load_full_drug_release.py
@@ -1,1 +1,31 @@
+# coding=utf-8
+"""Load the most recent full SNOMED UK drug release"""
 __author__ = 'ngurenyaga'
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    """Management command to load the newest full SNOMED UK drug release"""
+    help = 'Load the newest full ( bi-annual ) UK drug release'
+
+    def add_arguments(self, parser):
+        """Set the applicable command line arguments
+
+        :param parser:
+        """
+        # parser.add_argument('poll_id', nargs='+', type=int)
+        pass
+
+    def handle(self, *args, **options):
+        """The command's entry point"""
+        try:
+            # TODO - do the actual loading ( delegate to helpers )
+            pass
+        except:
+            # TODO - catch more specific exceptions
+            # TODO - return informative and specific error messages below
+            raise CommandError('Show a specific message here')
+
+        # TODO - write sensible feedback to standard out
+        self.stdout.write('Give user feedback here')
