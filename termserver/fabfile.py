d731d62a4c7dc2b316654288297fe5ed0e9c906a
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-11 13:12:15 +0300

    Added fabfile

---
+++ termserver/fabfile.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""Helpers - to reduce repetitive command line incantations"""
+__author__ = 'ngurenyaga'
+from os.path import dirname, abspath
+
+from fabric.api import local
+
+BASE_DIR = dirname(abspath(__file__))
+
+
+def reset():
+    """Spare the developer the dance of logging in as Postgres, dropping, creating, migrating etc"""
+    sudo = 'sudo -u postgres'
+    local('%s psql -c "DROP DATABASE IF EXISTS termserver"' % sudo)
+    local('%s psql -c "CREATE DATABASE termserver"' % sudo)
+    local('{}/manage.py clean_pyc'.format(BASE_DIR))
+    local('{}/manage.py migrate --noinput'.format(BASE_DIR))
+
+
+def run():
+    """Set up everything that needs to run e.g celery, celery beat, the Django application"""
+    # Start celery
+    # Start celery beat
+    # Start the application server
+    pass
+
+
+def test():
+    """A wrapper around the test invocation"""
+    pass
