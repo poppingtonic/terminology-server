e54da8aaeec43ee85fcd1e279da2b70de86031f2
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-14 15:00:10 +0300

    Simplified subsumption tester - will not load attempt to populate unpopulated database
    That concern can be handled elsewhere - maybe during application setup

--- termserver/core/subsumption.py
+++ termserver/core/subsumption.py
@@ -14,9 +14,7 @@ For the data that we get back from the query below:
 __author__ = 'ngurenyaga'
 
 from django.db import connection
-from django.core import management
 
-import sys
 import logging
 
 LOGGER = logging.getLogger(__name__)
@@ -28,8 +26,8 @@ class Tester(object):
         """
         The query below relies on the immutability of SNOMED IDs
         """
-        self.C2P_MAP = {}  # children to parents map
-        self.P2C_MAP = {}  # Parents to children map
+        self.CHILDREN_TO_PARENTS_MAP = {}  # children to parents map
+        self.PARENTS_TO_CHILDREN_MAP = {}  # Parents to children map
         self.CHILDREN_TO_PARENTS_KEY = "children_to_parents_map"
         self.PARENTS_TO_CHILDREN_KEY = "parents_to_children_map"
 
@@ -49,26 +47,10 @@ class Tester(object):
 
     def setup(self):
         """The actual data load"""
-        def _concepts_exist():
-            cur = connection.cursor()
-            connection.cursor().execute("SELECT count(*) FROM snomed_concept")
-            resp = cur.fetchall()
-            return resp and resp[0] and resp[0][0] and int(resp[0][0]) > 0
-
         if not self.setup_done:
-            if not _concepts_exist():
-                # Special case for tests; must be called only once
-                # Deliberately supporting only PostgreSQL
-                # TODO - Loading of SNOMED
-                # TODO - use materialized view
-                # TODO - do a vacuum after copy
-                # TODO - PYPY
-                pass
-
             # The actual transitive map computation
-            if not len(self.C2P_MAP) and not len(self.P2C_MAP):
-                # Will be lazily initialized
-                self.C2P_MAP, self.P2C_MAP = self.get_transitive_closure_map()
+            if not len(self.CHILDREN_TO_PARENTS_MAP) and not len(self.PARENTS_TO_CHILDREN_MAP):
+                self.CHILDREN_TO_PARENTS_MAP, self.PARENTS_TO_CHILDREN_MAP = self.get_transitive_closure_map()
 
             # Don't repeat setup
             self.setup_done = True
@@ -79,22 +61,22 @@ class Tester(object):
         cur.execute(self.IS_A_RELATIONSHIPS_QUERY)
         for (source_id, destination_id) in cur.fetchall():
             # Populate the children to parents map
-            if source_id in self.C2P_MAP:
-                if destination_id not in self.C2P_MAP[source_id]:
-                    self.C2P_MAP[
+            if source_id in self.CHILDREN_TO_PARENTS_MAP:
+                if destination_id not in self.CHILDREN_TO_PARENTS_MAP[source_id]:
+                    self.CHILDREN_TO_PARENTS_MAP[
                         source_id].append(destination_id)
             else:
-                self.C2P_MAP[source_id] = [destination_id]
+                self.CHILDREN_TO_PARENTS_MAP[source_id] = [destination_id]
 
             # Populate the parents to children map
-            if destination_id in self.P2C_MAP:
-                if source_id not in self.P2C_MAP[destination_id]:
-                    self.P2C_MAP[
+            if destination_id in self.PARENTS_TO_CHILDREN_MAP:
+                if source_id not in self.PARENTS_TO_CHILDREN_MAP[destination_id]:
+                    self.PARENTS_TO_CHILDREN_MAP[
                         destination_id].append(source_id)
             else:
-                self.P2C_MAP[destination_id] = [source_id]
+                self.PARENTS_TO_CHILDREN_MAP[destination_id] = [source_id]
 
-        return self.C2P_MAP, self.P2C_MAP
+        return self.CHILDREN_TO_PARENTS_MAP, self.PARENTS_TO_CHILDREN_MAP
 
     def get_children_of(self, parent_id):
         """Return the children and descendants of a concept
@@ -105,7 +87,7 @@ class Tester(object):
         output = []
 
         def _walk(current_child_id):
-            children = self.P2C_MAP.get(current_child_id, [])
+            children = self.PARENTS_TO_CHILDREN_MAP.get(current_child_id, [])
             output.extend(children)
             for next_parent_id in children:
                 _walk(next_parent_id)
@@ -119,7 +101,7 @@ class Tester(object):
         :param parent_id:
         """
         self.setup()
-        return self.P2C_MAP.get(parent_id, [])
+        return self.PARENTS_TO_CHILDREN_MAP.get(parent_id, [])
 
     def get_parents_of(self, child_id):
         """Return the parents and ancestors of a concept
@@ -130,7 +112,7 @@ class Tester(object):
         output = []
 
         def _walk(current_child_id):
-            parents = self.C2P_MAP.get(current_child_id, [])
+            parents = self.CHILDREN_TO_PARENTS_MAP.get(current_child_id, [])
             output.extend(parents)
             for next_child_id in parents:
                 _walk(next_child_id)
@@ -144,7 +126,7 @@ class Tester(object):
         :param child_id:
         """
         self.setup()
-        return self.C2P_MAP.get(child_id, [])
+        return self.CHILDREN_TO_PARENTS_MAP.get(child_id, [])
 
     def is_parent_of(self, child_id, candidate_parent_id):
         """
