f8325b11920944ef6036294232bab492f2ee9a87
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-16 10:41:02 +0300

    Minor refactor - modified core model structure to make room for mapping the materialized ( cache ) views

---
+++ termserver/core/models/__init__.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+"""Assemble models from the various files and present them as part of a unified 'models' module"""
+from .core_models import *
---
+++ termserver/core/models/unmanaged_models.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+"""These models map MATERIALIZED VIEWS that pre-compute expensive properties. The views are managed via migrations."""
+__author__ = 'ngurenyaga'
