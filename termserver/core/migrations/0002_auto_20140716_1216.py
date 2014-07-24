ab2ece52e9a20c8f199425b7d2263fbb0d64b5cd
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-24 11:59:59 +0300

    Added SQL to create SNOMED reference set referenced_component index

--- termserver/core/migrations/0002_auto_20140716_1216.py
+++ termserver/core/migrations/0002_auto_20140716_1216.py
@@ -14,10 +14,11 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             "CREATE EXTENSION plpythonu;"
-            " CREATE EXTENSION plv8;"
+            "CREATE EXTENSION plv8;"
             "CREATE INDEX concept_component_id_index ON snomed_concept(component_id);"
             "CREATE INDEX description_component_id_index ON snomed_description(component_id);"
             "CREATE index source_id_index ON snomed_relationship(source_id);"
             "CREATE index destination_id_index ON snomed_relationship(destination_id);"
+            "CREATE INDEX lang_refset_referenced_component ON snomed_language_reference_set(referenced_component_id)"
         ),
     ]
