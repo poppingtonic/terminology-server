5f330e6b1bd5c7cc8018fa0d4f803df123f362e3
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-24 18:02:09 +0300

    Added composite index on SNOMED concept to speed up GROUP BY in materialized view creation

--- termserver/core/migrations/0002_auto_20140716_1216.py
+++ termserver/core/migrations/0002_auto_20140716_1216.py
@@ -20,6 +20,7 @@ class Migration(migrations.Migration):
             "CREATE INDEX description_concept_id_index ON snomed_description(concept_id);"
             "CREATE index source_id_index ON snomed_relationship(source_id);"
             "CREATE index destination_id_index ON snomed_relationship(destination_id);"
-            "CREATE INDEX lang_refset_referenced_component ON snomed_language_reference_set(referenced_component_id)"
+            "CREATE INDEX lang_refset_referenced_component ON snomed_language_reference_set(referenced_component_id);"
+            "CREATE INDEX con_composite ON snomed_concept(component_id, effective_time, active, module_id, definition_status_id);"
         ),
     ]
