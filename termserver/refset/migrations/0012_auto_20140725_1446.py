b806eeed3caf7a8f8af78bfcfef6d634763af64d
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-25 14:47:44 +0300

    Added migration that creates query specification reference set expanded ( materialized ) view

---
+++ termserver/refset/migrations/0012_auto_20140725_1446.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    SQL = """
+    DROP MATERIALIZED VIEW IF EXISTS query_specification_reference_set_expanded_view;
+    CREATE MATERIALIZED VIEW query_specification_reference_set_expanded_view AS
+    SELECT
+      rf.id, rf.row_id, rf.effective_time, rf.active,
+      rf.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.module_id) AS module_name,
+      rf.refset_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.refset_id) AS refset_name,
+      rf.referenced_component_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.referenced_component_id) AS referenced_component_name,
+      rf.query
+    FROM snomed_query_specification_reference_set rf;
+    CREATE INDEX query_specification_reference_set_expanded_view_id ON query_specification_reference_set_expanded_view(id);
+    CREATE INDEX query_specification_reference_set_expanded_view_row_id ON query_specification_reference_set_expanded_view(row_id);
+    """
+
+    dependencies = [
+        ('refset', '0011_auto_20140725_1442'),
+    ]
+
+    operations = [
+        migrations.RunSQL(SQL),
+    ]
