35139bc134b3092fd0b477ec7b6ee09783bfeab5
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-25 12:55:54 +0300

    Added ( materialized ) view concept_expanded_view

--- termserver/refset/migrations/0004_auto_20140725_1233.py
+++ termserver/refset/migrations/0004_auto_20140725_1233.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     CREATE MATERIALIZED VIEW reference_set_descriptor_reference_set_expanded_view AS
     SELECT
       rf.id, rf.row_id, rf.effective_time, rf.active, rf.attribute_order,
-      rf.module_id,
+      rf.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.module_id) AS module_name,
       rf.refset_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.refset_id) AS refset_name,
       rf.referenced_component_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.referenced_component_id) AS referenced_component_name,
       rf.attribute_description_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.attribute_description_id) AS attribute_description_name,
