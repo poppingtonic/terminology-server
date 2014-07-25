1a60aa9519c454eb76670e0c45b2474f64810509
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-25 13:28:45 +0300

    Added migration to create materialized view for simple map reference sets

---
+++ termserver/refset/migrations/0008_auto_20140725_1327.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    SQL = """
+    DROP MATERIALIZED VIEW IF EXISTS simple_map_reference_set_expanded_view;
+    CREATE MATERIALIZED VIEW simple_map_reference_set_expanded_view AS
+    SELECT
+      rf.id, rf.row_id, rf.effective_time, rf.active,
+      rf.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.module_id) AS module_name,
+      rf.refset_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.refset_id) AS refset_name,
+      rf.referenced_component_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.referenced_component_id) AS referenced_component_name,
+      rf.map_target
+    FROM snomed_simple_map_reference_set rf;
+    CREATE INDEX simple_map_reference_set_expanded_view_id ON simple_map_reference_set_expanded_view(id);
+    CREATE INDEX simple_map_reference_set_expanded_view_row_id ON simple_map_reference_set_expanded_view(row_id);
+    """
+
+    dependencies = [
+        ('refset', '0007_auto_20140725_1313'),
+    ]
+
+    operations = [
+        migrations.RunSQL(SQL),
+    ]
