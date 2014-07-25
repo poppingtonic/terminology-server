4e76532472719e6c52e1dacac574a8e40bfa8c49
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-07-25 13:14:04 +0300

    Added migration to create expanded ( materialized ) view for attribute value reference set

--- termserver/refset/migrations/0007_auto_20140725_1313.py
+++ termserver/refset/migrations/0007_auto_20140725_1313.py
@@ -7,21 +7,21 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     SQL = """
-    DROP MATERIALIZED VIEW IF EXISTS ordered_reference_set_expanded_view;
-    CREATE MATERIALIZED VIEW ordered_reference_set_expanded_view AS
+    DROP MATERIALIZED VIEW IF EXISTS attribute_value_reference_set_expanded_view;
+    CREATE MATERIALIZED VIEW attribute_value_reference_set_expanded_view AS
     SELECT
-      rf.id, rf.row_id, rf.effective_time, rf.active, rf.order,
+      rf.id, rf.row_id, rf.effective_time, rf.active,
       rf.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.module_id) AS module_name,
       rf.refset_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.refset_id) AS refset_name,
       rf.referenced_component_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.referenced_component_id) AS referenced_component_name,
-      rf.linked_to_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.linked_to_id) AS linked_to_name
-    FROM snomed_ordered_reference_set rf;
-    CREATE INDEX ordered_reference_set_expanded_view_id ON ordered_reference_set_expanded_view(id);
-    CREATE INDEX ordered_reference_set_expanded_view_row_id ON ordered_reference_set_expanded_view(row_id);
+      rf.value_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.value_id) AS value_name
+    FROM snomed_attribute_value_reference_set rf;
+    CREATE INDEX attribute_value_reference_set_expanded_view_id ON attribute_value_reference_set_expanded_view(id);
+    CREATE INDEX attribute_value_reference_set_expanded_view_row_id ON attribute_value_reference_set_expanded_view(row_id);
     """
 
     dependencies = [
-        ('refset', '0005_auto_20140725_1254'),
+        ('refset', '0006_auto_20140725_1300'),
     ]
 
     operations = [
