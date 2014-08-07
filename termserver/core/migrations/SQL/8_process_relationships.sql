8e4a89af2bbc54b1ecd62d5724e0b8610fb0369d
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-08-07 08:17:06 +0300

    Fixed data loading

--- termserver/core/migrations/SQL/8_process_relationships.sql
+++ termserver/core/migrations/SQL/8_process_relationships.sql
@@ -3,7 +3,7 @@ import ujson as json
 
 def _get_preferred_name(concept_id):
     # Unable to use string interpolation because Django's SQL parser will choke on the percent sign
-    return plpy.execute("SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = " + str(concept_id[0]))["preferred_term"]
+    return plpy.execute("SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = " + str(concept_id))[0]["preferred_term"]
 
 def _process_relationship(relationship):
     rel = json.loads(relationship)
