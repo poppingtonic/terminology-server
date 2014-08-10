3b6f9999db4f9073a9116c08a73bfa7ea289d0f7
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-08-10 04:37:22 +0300

    Checkpoint - before attempting a different take on search

--- termserver/search/search_query.py
+++ termserver/search/search_query.py
@@ -7,3 +7,25 @@ def search():
     """Wrap the raw Elasticsearch operations"""
     searcher = ConceptMapping.search()
     # TODO Ensure that all queries have 25 items as default item number
+
+
+"""
+// attempt 1
+{
+  "query" : {
+    "match": {
+       "descriptions": {
+            "query": "myocardial infarction",
+           "type": "phrase",
+           "cutoff_frequency": 0.01,
+           "operator": "and",
+           "fuzziness": "AUTO",
+           "max_expansions": 3,
+          "slop": 2
+       }
+    }
+  }
+}
+
+
+"""
