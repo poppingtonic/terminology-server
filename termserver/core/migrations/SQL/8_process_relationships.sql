8ed20659f988b73836a3de6331ed1aff2155c8bb
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-08-28 10:56:10 +0300

    Fixed possible memory leak

--- termserver/core/migrations/SQL/8_process_relationships.sql
+++ termserver/core/migrations/SQL/8_process_relationships.sql
@@ -1,15 +1,19 @@
 CREATE OR REPLACE FUNCTION process_relationships(rels bigint[]) RETURNS text AS $$
 import ujson as json
+import gc
 
 def _get_preferred_name(concept_id):
     # Unable to use string interpolation because Django's SQL parser will choke on the percent sign
     return plpy.execute("SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = " + str(concept_id))[0]["preferred_term"]
 
-def _process_relationship(relationship):
+def _process_relationships():
     return [{
         "concept_id": rel,
         "concept_name": _get_preferred_name(rel)
     } for rel in rels]
 
-return json.dumps([_process_relationship(rel) for rel in rels])
+# Earlier versions had pathological memory usage ( crashing with failed allocations )
+gc.collect()
+
+return json.dumps(_process_relationships())
 $$ LANGUAGE plpythonu;
