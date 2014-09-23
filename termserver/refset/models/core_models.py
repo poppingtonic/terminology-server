e865dacf4e138d1464b15e9d8e8e8d1ac51ab625
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-09-23 18:33:33 +0300

    Moved models from refset/models/shared.py to refset/models/core_models.py
    Part of the process of unifying all refset models back into a single file

--- termserver/refset/models/core_models.py
+++ termserver/refset/models/core_models.py
@@ -1,7 +1,33 @@
 # -coding=utf-8
 """Models for SNOMED extension ( refset ) content"""
 from django.db import models
-from .shared import RefsetBase, ComplexExtendedMapReferenceSetBase
+from django_extensions.db.fields import PostgreSQLUUIDField
+
+
+class RefsetBase(models.Model):
+    """Abstract base model for all reference set types"""
+    row_id = PostgreSQLUUIDField(auto=False)
+    effective_time = models.DateField()
+    active = models.BooleanField(default=True)
+    module_id = models.BigIntegerField()
+    refset_id = models.BigIntegerField()
+    referenced_component_id = models.BigIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class ComplexExtendedMapReferenceSetBase(RefsetBase):
+    """Shared base class for both complex and extended reference sets"""
+    map_group = models.IntegerField()
+    map_priority = models.IntegerField()
+    map_rule = models.TextField()
+    map_advice = models.TextField()
+    map_target = models.CharField(max_length=256)
+    correlation_id = models.BigIntegerField()
+
+    class Meta:
+        abstract = True
 
 
 class SimpleReferenceSetFull(RefsetBase):
