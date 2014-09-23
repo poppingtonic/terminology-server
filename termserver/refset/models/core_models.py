e8096d58e44c1b4446d4b2511ecabe36f72545b8
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-09-23 18:37:14 +0300

    Moved dynamic snapshot refset models to the same file as core refset models

--- termserver/refset/models/core_models.py
+++ termserver/refset/models/core_models.py
@@ -131,3 +131,139 @@ class ReferenceSetDescriptorReferenceSetFull(RefsetBase):
     class Meta:
         db_table = 'snomed_reference_set_descriptor_reference_set_full'
         verbose_name = 'refset_descriptor_refset_full'
+
+
+class SimpleReferenceSetDynamicSnapshot(RefsetBase):
+    """Currently 'in force' rows of the simple reference sets"""
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_simple_reference_set'
+        verbose_name = 'simple_refset_snapshot'
+
+
+class OrderedReferenceSetDynamicSnapshot(RefsetBase):
+    """Dynamic snapshot of refset used to group components"""
+    order = models.PositiveSmallIntegerField()
+    linked_to_id = models.BigIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_ordered_reference_set'
+        verbose_name = 'ordered_refset_snapshot'
+
+
+class AttributeValueReferenceSetDynamicSnapshot(RefsetBase):
+    """Dynamic snapshot of refset used to tag components with values"""
+    value_id = models.BigIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_attribute_value_reference_set'
+        verbose_name = 'attribute_value_refset_snapshot'
+
+
+class SimpleMapReferenceSetDynamicSnapshot(RefsetBase):
+    """Currently 'in force' rows of the simple map reference sets"""
+    map_target = models.CharField(max_length=256)
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_simple_map_reference_set'
+        verbose_name = 'simple_map_refset_snapshot'
+
+
+class ComplexMapReferenceSetDynamicSnapshot(
+        ComplexExtendedMapReferenceSetBase):
+    """Currently 'in force' rows of the complex map reference sets"""
+    map_block = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_complex_map_reference_set'
+        verbose_name = 'complex_map_refset_snapshot'
+
+
+class ExtendedMapReferenceSetDynamicSnapshot(
+        ComplexExtendedMapReferenceSetBase):
+    """Like for complex map refsets, but with one additional field"""
+    map_category_id = models.BigIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_extended_map_reference_set'
+        verbose_name = 'extended_map_refset_snapshot'
+
+
+class LanguageReferenceSetDynamicSnapshot(RefsetBase):
+    """Currently 'in force' rows of the language reference sets"""
+    acceptability_id = models.BigIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_language_reference_set'
+        verbose_name = 'language_map_refset_snapshot'
+
+
+class QuerySpecificationReferenceSetDynamicSnapshot(RefsetBase):
+    """Currently 'in force' rows of the query specification reference sets"""
+    query = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_query_specification_reference_set'
+        verbose_name = 'query_spec_refset_snapshot'
+
+
+class AnnotationReferenceSetDynamicSnapshot(RefsetBase):
+    """Currently 'in force' rows of the annotation reference sets"""
+    annotation = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_annotation_reference_set'
+        verbose_name = 'annotation_refset_snapshot'
+
+
+class AssociationReferenceSetDynamicSnapshot(RefsetBase):
+    """Create associations between components e.g historical associations"""
+    target_component_id = models.BigIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_association_reference_set'
+        verbose_name = 'association_refset_snapshot'
+
+
+class ModuleDependencyReferenceSetDynamicSnapshot(RefsetBase):
+    """Specify dependencies between modules"""
+    source_effective_time = models.DateField()
+    target_effective_time = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_module_dependency_reference_set'
+        verbose_name = 'mod_dep_refset_snapshot'
+
+
+class DescriptionFormatReferenceSetDynamicSnapshot(RefsetBase):
+    """Provide format and length information for different description types"""
+    description_format_id = models.BigIntegerField()
+    description_length = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_description_format_reference_set'
+        verbose_name = 'description_format_refset_snapshot'
+
+
+class ReferenceSetDescriptorReferenceSetDynamicSnapshot(RefsetBase):
+    """Provide validation information for reference sets"""
+    attribute_description_id = models.BigIntegerField()
+    attribute_type_id = models.BigIntegerField()
+    attribute_order = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'snomed_reference_set_descriptor_reference_set'
+        verbose_name = 'refset_descriptor_refset_snapshot'
