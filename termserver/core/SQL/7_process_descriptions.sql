c81977c5e187cdb2aa54379702844113a2d2e450
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-10-15 15:09:16 +0300

    Checkpoint

--- termserver/core/SQL/7_process_descriptions.sql
+++ termserver/core/SQL/7_process_descriptions.sql
@@ -10,17 +10,10 @@ def _get_preferred_name(concept_id):
 
 
 def _process_description(descr):
-    return {
-        "description_id": descr["component_id"],
-        "type_id": descr["type_id"],
-        "type_name": _get_preferred_name(descr["type_id"]),
-        "module_id": descr["module_id"],
-        "module_name": _get_preferred_name(descr["module_id"]),
-        "case_significance_id": descr["case_significance_id"],
-        "case_significance_name": _get_preferred_name(descr["case_significance_id"]),
-        "term": descr["term"],
-        "active": descr["active"]
-    }
+    descr["type_name"] = _get_preferred_name(descr["type_id"])
+    descr["module_name"] = _get_preferred_name(descr["module_id"])
+    descr["case_significance_name"] = _get_preferred_name(descr["case_significance_id"])
+    return descr
 
 
 def _get_main_descriptions():
@@ -32,31 +25,19 @@ def _get_main_descriptions():
     definition = ''
     preferred_term = ''
 
-    for descr in [json.loads(descr) for descr in descs]:
-        # All descriptions
-        descrs.append(_process_description(descr))
-
-        # Preferred terms
-        if descr["acceptability_id"] == 900000000000548007:
-            preferred_terms.append(_process_description(descr))
-
-        # Synonyms
-        if descr["acceptability_id"] in [900000000000549004, None] and descr["type_id"] == 900000000000013009:
-            synonyms.append(_process_description(descr))
-
-        # Fully specified name
-        if descr["type_id"] == 900000000000003001:
+    for descr in [_process_description(json.loads(descr)) for descr in descs]:
+        descrs.append(descr)
+        if descr["type_id"] == 900000000000003001:  # Fully specified name
             fsn = descr["term"]
-
-        # Definition
-        if descr["type_id"] == 900000000000550004:
+        elif descr["type_id"] == 900000000000550004:  # Definition
             definition = descr["term"]
-
-        # Preferred term; we record the first one, but the UK one takes precedence
-        if descr["acceptability_id"] == 900000000000548007 and not preferred_term:
-            preferred_term = descr["term"]
-        if descr["acceptability_id"] == 900000000000548007 and descr["refset_id"] == 999001251000000103:
-            preferred_term = descr["term"]
+        elif descr["type_id"] == 900000000000013009:  # Synonym
+            if descr["acceptability_id"] == 900000000000548007:
+                preferred_terms.append(descr)
+                if descr["refset_id"] == 999001251000000103:  # UK refset
+                    preferred_term = descr["term"]
+            else:
+                synonyms.append(descr)
 
     return (
         json.dumps(descrs),
--- termserver/core/SQL/9_con_desc_cte.sql
+++ termserver/core/SQL/9_con_desc_cte.sql
@@ -4,7 +4,22 @@ SELECT
     conc.id as id, conc.component_id AS concept_id,
     conc.effective_time, conc.active, conc.module_id, conc.definition_status_id,
     CASE WHEN conc.definition_status_id = 900000000000074008 THEN true ELSE false END AS is_primitive,
-    array_agg(row_to_json((des.component_id, des.module_id, des.type_id, des.effective_time, des.case_significance_id, des.term, des.language_code, des.active, ref.acceptability_id, ref.refset_id)::description)) AS descs
+    array_agg(
+        row_to_json(
+            (
+                des.component_id,
+                des.module_id,
+                des.type_id,
+                des.effective_time,
+                des.case_significance_id,
+                des.term,
+                des.language_code,
+                des.active,
+                ref.acceptability_id,
+                ref.refset_id
+            )::description
+        )
+    ) AS descs
   FROM snomed_concept conc
   LEFT JOIN snomed_description des ON des.concept_id = conc.component_id
   LEFT JOIN snomed_language_reference_set ref ON ref.referenced_component_id = des.component_id
