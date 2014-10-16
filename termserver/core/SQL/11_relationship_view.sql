6db3a731e4abd42249bd486ee0c369e908c35c71
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-10-16 07:53:35 +0300

    More uses of get_concept_preferred_term SQL function
    Replaces cases where "plain queries" were used

--- termserver/core/SQL/11_relationship_view.sql
+++ termserver/core/SQL/11_relationship_view.sql
@@ -1,10 +1,10 @@
 CREATE MATERIALIZED VIEW relationship_expanded_view AS
   SELECT
     rel.id, rel.component_id, rel.effective_time, rel.active, rel.relationship_group,
-    rel.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rel.module_id) AS module_name,
-    rel.source_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rel.source_id) AS source_name,
-    rel.destination_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rel.destination_id) AS destination_name,
-    rel.type_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rel.type_id) AS type_name,
-    rel.characteristic_type_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rel.characteristic_type_id) AS characteristic_type_name,
-    rel.modifier_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rel.modifier_id) AS modifier_name
+    rel.module_id, get_concept_preferred_term(rel.module_id) AS module_name,
+    rel.source_id, get_concept_preferred_term(rel.source_id) AS source_name,
+    rel.destination_id, get_concept_preferred_term(rel.destination_id) AS destination_name,
+    rel.type_id, get_concept_preferred_term(rel.type_id) AS type_name,
+    rel.characteristic_type_id, get_concept_preferred_term(rel.characteristic_type_id) AS characteristic_type_name,
+    rel.modifier_id, get_concept_preferred_term(rel.modifier_id) AS modifier_name
   FROM snomed_relationship rel;
--- termserver/core/SQL/12_description_view.sql
+++ termserver/core/SQL/12_description_view.sql
@@ -1,8 +1,8 @@
 CREATE MATERIALIZED VIEW description_expanded_view AS
   SELECT
     descr.id, descr.component_id, descr.effective_time, descr.active, descr.language_code, descr.term,
-    descr.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = descr.module_id) AS module_name,
-    descr.concept_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = descr.concept_id) AS concept_name,
-    descr.type_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = descr.type_id) AS type_name,
-    descr.case_significance_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = descr.case_significance_id) AS case_significance_name
+    descr.module_id, get_concept_preferred_term(descr.module_id) AS module_name,
+    descr.concept_id, get_concept_preferred_term(descr.concept_id) AS concept_name,
+    descr.type_id, get_concept_preferred_term(descr.type_id) AS type_name,
+    descr.case_significance_id, get_concept_preferred_term(descr.case_significance_id) AS case_significance_name
   FROM snomed_description descr;
