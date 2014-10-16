8d3f7779966cdb897be9965319e4652a065fb2a4
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-10-16 08:40:13 +0300

    Pure SQL implementation of get_preferred_term

--- termserver/core/SQL/4_get_preferred_term.sql
+++ termserver/core/SQL/4_get_preferred_term.sql
@@ -1,13 +1,11 @@
-CREATE OR REPLACE FUNCTION get_preferred_term(descs json[]) RETURNS text AS $$
-import ujson as json
+CREATE TYPE shortened_description AS (
+    term text,
+    acceptability_id bigint,
+    refset_id bigint
+);
 
-for descr in descs:
-    desc_row = json.loads(descr)
-    # Record the first preferred term that we see, but allow the UK language refset to override
-    if desc_row["acceptability_id"] == 900000000000548007 and not preferred_term:
-        preferred_term = desc_row["term"]
-    elif desc_row["acceptability_id"] == 900000000000548007 and desc_row["refset_id"] == 999001251000000103:
-        preferred_term = desc_row["term"]
-
-return preferred_term
-$$ LANGUAGE plpythonu;
+CREATE OR REPLACE FUNCTION get_preferred_term(shortened_description[]) RETURNS text AS $$
+    SELECT term FROM unnest($1)
+    WHERE acceptability_id = 900000000000548007
+    AND refset_id = 999001251000000103;
+$$ LANGUAGE SQL;
--- termserver/refset/SQL/1_reference_set_descriptor_reference_set_expanded_view.sql
+++ termserver/refset/SQL/1_reference_set_descriptor_reference_set_expanded_view.sql
@@ -1,9 +1,9 @@
 CREATE MATERIALIZED VIEW reference_set_descriptor_reference_set_expanded_view AS
   SELECT
     rf.id, rf.row_id, rf.effective_time, rf.active, rf.attribute_order,
-    rf.module_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.module_id) AS module_name,
-    rf.refset_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.refset_id) AS refset_name,
-    rf.referenced_component_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.referenced_component_id) AS referenced_component_name,
-    rf.attribute_description_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.attribute_description_id) AS attribute_description_name,
-    rf.attribute_type_id, (SELECT preferred_term FROM concept_preferred_terms WHERE concept_id = rf.attribute_type_id) AS attribute_type_name
+    rf.module_id, get_concept_preferred_term(rf.module_id) AS module_name,
+    rf.refset_id, get_concept_preferred_term(rf.refset_id) AS refset_name,
+    rf.referenced_component_id, get_concept_preferred_term(rf.referenced_component_id) AS referenced_component_name,
+    rf.attribute_description_id, get_concept_preferred_term(rf.attribute_description_id) AS attribute_description_name,
+    rf.attribute_type_id, get_concept_preferred_term(rf.attribute_type_id) AS attribute_type_name
   FROM snomed_reference_set_descriptor_reference_set rf;
