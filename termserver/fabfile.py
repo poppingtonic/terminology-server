poppingtonic/terminology-server

commit 8b62cf652d44468c5192f30de5548be08a8452d7

Checkpoint - before implementing Dropbox support

termserver/fabfile.py
@@ -7,7 +7,6 @@ from django.conf import settings
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-BASE_DIR = settings.BASE_DIR
 
 from administration.management.commands.shared.load import (
     refresh_materialized_views,
@@ -21,21 +20,21 @@ def reset():
     sudo = 'sudo -u postgres'
     local('%s psql -c "DROP DATABASE IF EXISTS termserver"' % sudo)
     local('%s psql -c "CREATE DATABASE termserver"' % sudo)
-    local('{}/manage.py clean_pyc'.format(BASE_DIR))
-    local('{}/manage.py migrate --noinput'.format(BASE_DIR))
+    local('{}/manage.py clean_pyc'.format(settings.BASE_DIR))
+    local('{}/manage.py migrate --noinput'.format(settings.BASE_DIR))
 
 
 @task
 def run():
     """Run the various services that are needed"""
-    local('{}/manage.py runserver'.format(BASE_DIR))
-    local('{}/celery -A config worker -l info'.format(BASE_DIR))
+    local('{}/manage.py runserver'.format(settings.BASE_DIR))
+    local('{}/celery -A config worker -l info'.format(settings.BASE_DIR))
 
 
 @task
 def load_snomed():
     """Helper to make this repetitive task less dreary"""
-    local('{}/manage.py load_full_release'.format(BASE_DIR))
+    local('{}/manage.py load_full_release'.format(settings.BASE_DIR))
 
 
 @task
@@ -53,13 +52,13 @@ def refresh_views():
 @task
 def index():
     """Rebuild the SNOMED concept search index"""
-    local('{}/manage.py elasticsearch_index'.format(BASE_DIR))
+    local('{}/manage.py elasticsearch_index'.format(settings.BASE_DIR))
 
 
 @task
 def backup():
     """Export all custom SIL content and also back it up online"""
-    # Track the export folder in GIT; it should be small enough for that
+    # TODO Track the export folder in GIT; it should be small enough for that
     pass
 
 
@@ -91,6 +90,7 @@ def build():
     backup()
     reset_and_load()
     index()
+    # TODO Produce a docker image
 
 
 @task(default=True)

termserver/administration/management/commands/fetch_snomed_content_from_dropbox.py
@@ -1,0 +1,24 @@
+# coding=utf-8
+"""Get the content from a predefined Dropbox folder"""
+__author__ = 'ngurenyaga'
+import dropbox
+
+from django.core.management.base import BaseCommand
+
+# The bulky SNOMED content is kept on DropBox ( too big for GIT )
+SNOMED_FOLDER_URL = \
+    'https://www.dropbox.com/sh/zxg5mkeibq2sp14/AACSO2Bc4iNyi27pxcWAwNb3a?dl=0'
+DROPBOX_APP_KEY = 'fyukmm3h0a65ssv'
+DROPBOX_APP_SECRET = 'oslxpyyajgcoqqi'
+DROPBOX_ACCESS_TOKEN = \
+    'eriIgWvfTBQAAAAAAAAHrOy2aZxAzWpeu-CI6XsmzM0zBmT5LqpdkygcLM1SIs1y'
+
+
+class Command(BaseCommand):
+    """The SNOMED content is too large to be distributed via GIT"""
+    help = 'Fetch the current SNOMED content from Dropbox'
+
+    def handle(self, *args, **options):
+        """The command's entry point"""
+        client = dropbox.client.DropboxClient(DROPBOX_ACCESS_TOKEN)
+

termserver/search/search_index.py
@@ -92,8 +92,3 @@ def bulk_index():
 
     # Refresh the index after bulk load ( at the end )
     es.indices.refresh(INDEX_NAME)
-
-
-
-# TODO Create a "fab backup" step that works with Google Object storage
-# TODO Create a docker container build process; be sure to start Celery too

commit b6011d4cc99c052f21feb71162a73366d4de486c

Fixed bug introduced into fabfile by commit 8b62cf652d44468c5192f30de5548be08a8452d7
The use pattern for settings.BASE_DIR was faulty

termserver/fabfile.py
@@ -11,6 +11,8 @@ from administration.management.commands.shared.load import (
 )
 from django.conf import settings
 
+BASE_DIR = settings.BASE_DIR
+
 
 @task
 def reset():
@@ -18,21 +20,21 @@ def reset():
     sudo = 'sudo -u postgres'
     local('%s psql -c "DROP DATABASE IF EXISTS termserver"' % sudo)
     local('%s psql -c "CREATE DATABASE termserver"' % sudo)
-    local('{}/manage.py clean_pyc'.format(settings.BASE_DIR))
-    local('{}/manage.py migrate --noinput'.format(settings.BASE_DIR))
+    local('{}/manage.py clean_pyc'.format(BASE_DIR))
+    local('{}/manage.py migrate --noinput'.format(BASE_DIR))
 
 
 @task
 def run():
     """Run the various services that are needed"""
-    local('{}/manage.py runserver'.format(settings.BASE_DIR))
-    local('{}/celery -A config worker -l info'.format(settings.BASE_DIR))
+    local('{}/manage.py runserver'.format(BASE_DIR))
+    local('{}/celery -A config worker -l info'.format(BASE_DIR))
 
 
 @task
 def load_snomed():
     """Helper to make this repetitive task less dreary"""
-    local('{}/manage.py load_full_release'.format(settings.BASE_DIR))
+    local('{}/manage.py load_full_release'.format(BASE_DIR))
 
 
 @task
@@ -50,7 +52,7 @@ def refresh_views():
 @task
 def index():
     """Rebuild the SNOMED concept search index"""
-    local('{}/manage.py elasticsearch_index'.format(settings.BASE_DIR))
+    local('{}/manage.py elasticsearch_index'.format(BASE_DIR))
 
 
 @task
@@ -104,4 +106,4 @@ def rebuild():
 def retrieve_terminology_data():
     """Retrieve the terminology archive and extract it"""
     local('{}/manage.py fetch_snomed_content_from_dropbox'.format(
-          settings.BASE_DIR))
+          BASE_DIR))