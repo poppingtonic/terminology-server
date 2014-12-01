588cffa00ad6cd59c9ccb143845d34eeb3d0de6f
Dr Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2014-12-01 19:39:58 +0300

    Set up build step in tests
    When running on CI, it will load a fresh database

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,18 @@ DATABASES = {
         'CONN_MAX_AGE': 900
     }
 }
+if os.getenv('CIRCLECI'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'circle_test',
+            'USER': 'ubuntu',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            'CONN_MAX_AGE': 900
+        }
+    }
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Nairobi'
 USE_I18N = True
