3c5db1f41ddb5395f52d63646e1b92a12535a69d
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-03-17 11:40:10 +0300

    Removed api app
    Unified into core app

--- core/fields.py
+++ core/fields.py
@@ -1,5 +1,23 @@
 """See composite type registration in core/apps.py"""
 from django.db import models
+from rest_framework import serializers
+
+import json
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class DictField(serializers.Field):
+    """Read only field to handle DictFields used in denormalized views"""
+    def to_representation(self, obj):
+        try:
+            l = [json.loads(item) for item in obj]
+            LOGGER.debug('Object %s native representation is: %s' % (obj, l))
+            return l
+        except:
+            LOGGER.debug('Unable to load from JSON obj %s' % obj)
+            return obj
 
 
 class DenormalizedDescriptionField(models.Field):
