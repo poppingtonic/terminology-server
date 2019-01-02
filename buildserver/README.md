60b832cd419f80771eff5f716c840901918a4222
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2019-01-02 18:15:43 +0300

    Fix build server
    The build server now works again (early 2019)

---
+++ buildserver/README.md
@@ -0,0 +1,136 @@
+# SNOMED CT Terminology Buildserver and API
+This repository hosts the SIL SNOMED buildserver.
+
+The `buildserver` project runs a full build of the SNOMED CT release files
+downloaded from the Technology Reference data Update Distribution site:
+[Clinical](https://isd.hscic.gov.uk/trud3/user/authenticated/group/2/pack/26/subpack/102/releases)
+and
+[Drugs](https://isd.hscic.gov.uk/trud3/user/authenticated/group/2/pack/26/subpack/106/releases).
+
+These files are two zipfiles containing the International clinical release
+files and UK clinical extension files, and the UK drug extension.
+
+The end result is a list of compressed files, stored in the Google Cloud
+Storage bucket:
+[snomedct-terminology-build-data](https://console.cloud.google.com/storage/browser/snomedct-terminology-build-data/?project=savannah-emr).
+
+We should build SNOMED every two weeks, to follow the release schedule of the
+UK drug extension.
+
+## Key settings
+The main settings (and env variables) assembly file is deploy/settings.py .
+
+When debugging, you may not want the deploy server to be deleted when builds
+fail. In that case, set DELETE_ON_FAILED_BUILD and DELETE_ON_SUCCESSFUL_BUILD
+to False. When done debugging, remember to flip them to True.
+
+## How To Build SNOMED
+
+1. SNOMED CT is released on a rolling schedule of six months for
+clinical releases, and two weeks for drug releases in the UK. Since we
+rely on the terminology files released by the UK's National Health
+Service, we will track the UK's release schedule.
+
+The
+[Technology Reference data Update Distribution site](https://isd.hscic.gov.uk/trud3/user/authenticated/group/2/pack/26)
+hosts the release files, and you should go to that url to download the
+release files.
+
+- [ ] Look for 'UK SNOMED CT Drug Extension, RF2: Full' and click
+'Download releases'
+
+- [ ] Look for 'UK SNOMED CT Clinical Edition, RF2: Full' and click
+'Download releases'
+
+- [ ] *IMPORTANT NOTE* You'll then need to get access to a Dropbox
+app. You'll need a DropBox token and a set of app keys, which are in
+an ansible vault. Check the project's wiki for a password to the
+ansible vault.
+
+- [ ] Check the app folder (Dropbox/Apps/SNOMED/downloads/) for any
+existing release packages. Delete any packages in there before you add
+anything new.
+
+- [ ] Copy the downloaded release ZIP files into the app folder
+(Dropbox/Apps/SNOMED/downloads/).
+
+2. Clone this repository:
+
+3. Initialize the buildserver commands.
+
+This step requires that your machine can build the `cryptography` package,
+     to make Ansible run *fast*.
+
+     The `initialize.sh` script will install this, and its requirements for you.
+
+     *NOTE* Read the recommendations below if you'd like guidance on the
+     initialization process. Otherwise, the assumption is that you're
+     using Ubuntu 18.04 and above as your developer OS. We also assume
+     that you're using bash.
+
+### Recommendations
+     + Use bash.
+     + `profile completion`: Enable profile completion.
+     + `rc file`: We assume that you're using bash, so `~/.bashrc` (the default option)
+     + If there are network issues, just run the script again and it'll rebuild the environment.
+     + Set the zone `europe`.
+     + Set the region `europe-west1-d`.
+     + You'll get shell autocomplete if you select it from the command line.
+     + Select the gcloud project as `savannah-emr`.
+     + If you add any new environment variables, you'll need to `source ~/.bashrc` or whatever .rc file handles your shell environment.
+     + Any modifications to the buildserver deploy scripts will require
+     you to reinitialize the package if you don't want to create your own
+     virtualenv. This is because the deploy scripts are intended to run
+     using a different version of python than the python3 used to run the
+     whole thing, since Ansible doesn't support python 3.
+
+     Run the initialization script to setup the buildserver dependencies.
+
+     `./initialize.sh buildserver`
+
+     *IMPORTANT NOTE*: If this command takes you to a page to create a
+     google cloud account, you need to ask one of the `savannah-emr`
+     project admins to invite you to the project. *Do that before doing anything else, or everything
+     after this will fail*.
+
+     4. Source the environment variables from
+     [this page](https://github.com/savannahinformatics/slade360-terminology-server/wiki/Keys-and-Environment-Variables-To-Deploy-Terminology-Server).
+Put them into your shell configuration (`~/.bashrc` or `~/.zshrc`)
+    and restart your shell, or `source ~/.bashrc`/`source ~/.
+
+    5. Create the buildserver instance.
+
+    ```bash 
+
+    cd buildserver
+    build/buildserver create
+    ```
+
+    This creates a 4-core pre-emptible instance with 16GB RAM, 500GB SSD,
+    optimized to be a postgresql server. This machine runs Ubuntu 18.04,
+    so all software installed in it is optimized for a debian-based
+    OS. There are speed/storage tradeoffs made to achieve this, which
+    inform the choice to use a 500GB SSD.
+
+    SSD Persistent Disk volumes reach the per-VM limit of 15000 random
+    read IOPS at 333 GB. SSD Persistent Disk volumes reach the per-VM
+    limit of 15000 random write IOPS at 500 GB.
+
+    We max out the read/write speeds at 500 GB, so that's the minimum for
+    our purposes. This disk only lasts about an hour, every two weeks, so
+    the cost is minimized.
+
+    It requires full API access.
+
+    6. Deploy the buildserver.
+    `build/buildserver deploy`
+
+    Before deploying, install python (Python 2) on the host, with
+    sudo apt intall python. This is because recent Ubuntu cloud images do not
+    have Python 2 installed by default.
+
+    7. `build/buildserver delete`
+
+    This buildserver machine is *very expensive* to run, so we need to
+    terminate it automatically every time the deploy succeeds. The machine
+    is also deleted automatically if the deploy fails.
--- buildserver/commands/dropbox_content.py
+++ buildserver/commands/dropbox_content.py
@@ -16,18 +16,16 @@ from sil_snomed_server.config import config
 logging.captureWarnings(True)
 
 # The bulky SNOMED content is kept on DropBox ( too big for GIT )
-DROPBOX_SNOMED_FOLDER_URL = os.environ.get('DROPBOX_SNOMED_FOLDER_URL', '')
-DROPBOX_APP_KEY = os.environ.get('DROPBOX_APP_KEY', '')
-DROPBOX_APP_SECRET = os.environ.get('DROPBOX_APP_SECRET', '')
-DROPBOX_ACCESS_TOKEN = os.environ.get('DROPBOX_ACCESS_TOKEN', '')
+DROPBOX_SNOMED_FOLDER_URL = os.environ.get("DROPBOX_SNOMED_FOLDER_URL", "")
+DROPBOX_APP_KEY = os.environ.get("DROPBOX_APP_KEY", "")
+DROPBOX_APP_SECRET = os.environ.get("DROPBOX_APP_SECRET", "")
+DROPBOX_ACCESS_TOKEN = os.environ.get("DROPBOX_ACCESS_TOKEN", "")
 
-WORKING_FOLDER = os.path.join(
-    config.basedir,
-    'data/source_terminology_data'
-)
+WORKING_FOLDER = os.path.join(config.basedir, "data/source_terminology_data")
 EXTRACT_WORKING_FOLDER = os.path.join(
-    config.basedir, 'data/extracted_terminology_data')
-METADATA_FILE = os.path.join(WORKING_FOLDER, 'metadata.json')
+    config.basedir, "data/extracted_terminology_data"
+)
+METADATA_FILE = os.path.join(WORKING_FOLDER, "metadata.json")
 
 LOGGER = logging.getLogger(__name__)
 
@@ -39,59 +37,76 @@ if not os.path.exists(EXTRACT_WORKING_FOLDER):
 
 
 FILE_PATTERNS = {
-    'concepts': re.compile(r'^.*sct2_Concept_.+txt$'),
-    'descriptions': re.compile(r'^.*sct2_Description_.+txt$'),
-    'relationships': re.compile(r'^.*sct2_Relationship_.+txt$'),
-    'text_definitions': re.compile(r'^.*sct2_TextDefinition_.+txt$'),
-    'identifiers': re.compile(r'^.*sct2_Identifier_.+txt$'),
-    'stated_relationships': re.compile(r'^.*sct2_StatedRelationship_.+txt$'),
-    'simple_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+SimpleFull.+txt$'),
-    'ordered_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+OrderedFull.+txt$'),
-    'attribute_value_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+AttributeValueFull.+txt$'),
-    'simple_map_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+SimpleMapFull.+txt$'),
-    'complex_map_int_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+ComplexMapFull_INT.+txt$'),
-    'complex_map_gb_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+ComplexMapFull_GB.+txt$'),
-    'extended_map_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+ExtendedMapFull.+txt$'),
-    'language_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+LanguageFull.+txt$'),
-    'query_specification_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+QuerySpecificationFull.+txt$'),
-    'annotation_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+AnnotationFull.+txt$'),
-    'association_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+AssociationReferenceFull.+txt$'),
-    'module_dependency_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+ModuleDependencyFull.+txt$'),
-    'description_format_reference_sets':
-    re.compile(r'^.*der2_.*Refset.+DescriptionFormatFull.+txt$'),
-    'refset_descriptor_reference_sets':
-    re.compile(r'^.*der2_.*Refset.*RefsetDescriptorFull.+txt$'),
-    'description_type_reference_sets':
-    re.compile(r'^.*der2_.*Refset.*DescriptionTypeFull.+txt$')
+    "concepts": re.compile(r"^.*sct2_Concept_.+txt$"),
+    "descriptions": re.compile(r"^.*sct2_Description_.+txt$"),
+    "relationships": re.compile(r"^.*sct2_Relationship_.+txt$"),
+    "text_definitions": re.compile(r"^.*sct2_TextDefinition_.+txt$"),
+    "identifiers": re.compile(r"^.*sct2_Identifier_.+txt$"),
+    "stated_relationships": re.compile(r"^.*sct2_StatedRelationship_.+txt$"),
+    "simple_reference_sets": re.compile(r"^.*der2_.*Refset.+SimpleFull.+txt$"),
+    "ordered_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+OrderedFull.+txt$"
+    ),
+    "attribute_value_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+AttributeValueFull.+txt$"
+    ),
+    "simple_map_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+SimpleMapFull.+txt$"
+    ),
+    "complex_map_int_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+ComplexMapFull_INT.+txt$"
+    ),
+    "complex_map_gb_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+ComplexMapFull_GB.+txt$"
+    ),
+    "extended_map_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+ExtendedMapFull.+txt$"
+    ),
+    "language_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+LanguageFull.+txt$"
+    ),
+    "query_specification_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+QuerySpecificationFull.+txt$"
+    ),
+    "annotation_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+AnnotationFull.+txt$"
+    ),
+    "association_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+AssociationReferenceFull.+txt$"
+    ),
+    "module_dependency_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+ModuleDependencyFull.+txt$"
+    ),
+    "description_format_reference_sets": re.compile(
+        r"^.*der2_.*Refset.+DescriptionFormatFull.+txt$"
+    ),
+    "refset_descriptor_reference_sets": re.compile(
+        r"^.*der2_.*Refset.*RefsetDescriptorFull.+txt$"
+    ),
+    "description_type_reference_sets": re.compile(
+        r"^.*der2_.*Refset.*DescriptionTypeFull.+txt$"
+    ),
 }
 
 
 class DropboxData(object):
     """The SNOMED content is too large to be distributed via GIT"""
-    help = 'Fetch the current SNOMED content from Dropbox'
+
+    help = "Fetch the current SNOMED content from Dropbox"
 
     def __init__(self):
         try:
             self.client = dropbox.dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
-            self.upstream_metadata = self.client.files_list_folder('/downloads')
+            self.upstream_metadata = self.client.files_list_folder(
+                "/downloads"
+            )
             self.stored_metadata = self.get_stored_metadata()
             self.path_keyed_upstream_metadata = [
-                {'path':entry.path_display,
-                 'server_modified':entry.server_modified.strftime('%c'),
-                 'id':entry.id,
-                 'rev':entry.rev
+                {
+                    "path": entry.path_display,
+                    "server_modified": entry.server_modified.strftime("%c"),
+                    "id": entry.id,
+                    "rev": entry.rev,
                 }
                 for entry in self.upstream_metadata.entries
             ]
@@ -114,70 +129,74 @@ class DropboxData(object):
         # Extract the last part of the filename from the path
         try:
             new_file_name = os.path.basename(file_path)
-            new_file_path = os.path.join(
-                WORKING_FOLDER, new_file_name)
+            new_file_path = os.path.join(WORKING_FOLDER, new_file_name)
             self.client.files_download_to_file(new_file_path, file_path)
 
-            LOGGER.info('Downloaded %s from Dropbox' % new_file_path)
+            LOGGER.info("Downloaded %s from Dropbox" % new_file_path)
         except:
-            LOGGER.info('Unable to fetch %s from Dropbox' % file_path)
+            LOGGER.info("Unable to fetch %s from Dropbox" % file_path)
             try:
                 os.remove(new_file_path)
-                LOGGER.info('Deleted %s [ clean up ]' % new_file_path)
+                LOGGER.info("Deleted %s [ clean up ]" % new_file_path)
             except:
-                LOGGER.info('Cannot delete %s [ clean up ]' % new_file_path)
+                LOGGER.info("Cannot delete %s [ clean up ]" % new_file_path)
 
     def save_metadata(self, metadata_dict):
         """Save the newest metadata to file"""
-        with open(METADATA_FILE, 'w') as f:
+        with open(METADATA_FILE, "w") as f:
             f.write(json.dumps(metadata_dict, indent=4))
-        LOGGER.info('Saved metadata')
+        LOGGER.info("Saved metadata")
 
     def extract_file_paths(self, metadata_dict):
         """Get the file paths of the zipfiles from the metadata dict"""
         if metadata_dict is not []:
-            return map(lambda elt: elt['path'], metadata_dict)
+            return map(lambda elt: elt["path"], metadata_dict)
         else:
             return []
 
     def file_has_changed(self, file_path):
         """Return True if a file has changed, otherwise False"""
-        LOGGER.debug('Checking if %s has changed' % file_path)
-        if file_path not in self.extract_file_paths(self.path_keyed_upstream_metadata):
-            raise Exception('Inconsistent state; should not occur')
+        LOGGER.debug("Checking if %s has changed" % file_path)
+        if file_path not in self.extract_file_paths(
+            self.path_keyed_upstream_metadata
+        ):
+            raise Exception("Inconsistent state; should not occur")
 
-        if file_path not in self.extract_file_paths(self.path_keyed_stored_metadata):
-            LOGGER.info('%s appears to be a new file' % file_path)
+        if file_path not in self.extract_file_paths(
+            self.path_keyed_stored_metadata
+        ):
+            LOGGER.info("%s appears to be a new file" % file_path)
             return True  # We have not seen this file before
 
         expected_file_path = os.path.join(
-            WORKING_FOLDER, os.path.basename(file_path))
-        LOGGER.debug('Expected file path: %s' % expected_file_path)
+            WORKING_FOLDER, os.path.basename(file_path)
+        )
+        LOGGER.debug("Expected file path: %s" % expected_file_path)
         if not os.path.exists(expected_file_path):
-            LOGGER.info('Cannot find expected file %s' % expected_file_path)
+            LOGGER.info("Cannot find expected file %s" % expected_file_path)
             return True
-
 
     def metadata_has_changed(self):
         """Return True if there is a change; otherwise False"""
         if not self.stored_metadata:
-            LOGGER.info('No stored metadata; assumed to be a new installation')
+            LOGGER.info("No stored metadata; assumed to be a new installation")
             return True  # We assume that it is a new installation
         elif os.listdir(EXTRACT_WORKING_FOLDER) == []:
             LOGGER.info(
-                'The dest folder - %s - is empty, downloading/extracting' %
-                EXTRACT_WORKING_FOLDER)
+                "The dest folder - %s - is empty, downloading/extracting"
+                % EXTRACT_WORKING_FOLDER
+            )
             return True
         else:
             upstream_contents = self.path_keyed_upstream_metadata
             stored_contents = self.path_keyed_stored_metadata
-            if (len(upstream_contents) != len(stored_contents)):
-                LOGGER.info('The no of files is different upstream/downstream')
+            if len(upstream_contents) != len(stored_contents):
+                LOGGER.info("The no of files is different upstream/downstream")
                 return True  # There has been a change in the number of files
 
             for entry in upstream_contents:
-                if self.file_has_changed(entry['path']):
-                    LOGGER.info('%s has changed' % entry['path'])
+                if self.file_has_changed(entry["path"]):
+                    LOGGER.info("%s has changed" % entry["path"])
                     return True  # If any file has changed, exit early
 
         # Default exit
@@ -185,18 +204,15 @@ class DropboxData(object):
 
     def get_upstream_file_paths(self):
         """Produce an iterator of paths to non-dir files upstream"""
-        return (
-            entry['path'] for entry in self.path_keyed_upstream_metadata
-        )
+        return (entry["path"] for entry in self.path_keyed_upstream_metadata)
 
     def get_cached_file_paths(self):
         """Produce an iterator of paths to non-dir files in the local folder"""
         # In a fresh install, this will initially have been null
         if not self.stored_metadata:
             self.stored_metadata = self.get_stored_metadata()
-        return (
-            entry['path'] for entry in self.path_keyed_stored_metadata
-        )
+
+        return (entry["path"] for entry in self.path_keyed_stored_metadata)
 
     def ensure_dest_subfolder_exists(self, subfolder_name):
         """Guarantee that the subfolder exists before we write into it"""
@@ -206,47 +222,50 @@ class DropboxData(object):
 
         # Confirm that it is a directory
         if not os.path.isdir(subfolder_path):
-            raise Exception('%s ought to be a directory' % subfolder_path)
+            raise Exception("%s ought to be a directory" % subfolder_path)
 
     def save_zip_entry(self, zipfile, zip_entry):
         """Extract the entry to the correct location"""
-        full_release_pattern = re.compile(r'(.*)RF2Release(.*)Full/(.*)txt$')
-
-        if zip_entry.endswith('.txt') and full_release_pattern.match(zip_entry):
+        release_pattern = re.compile(r"(.*)RF2(.*)/(.*)txt$")
+        if zip_entry.endswith(".txt") and release_pattern.match(zip_entry):
             for folder_name, pattern in iter(FILE_PATTERNS.items()):
                 self.ensure_dest_subfolder_exists(folder_name)
                 if pattern.match(zip_entry):
                     file_name = os.path.basename(zip_entry)
                     subfolder_path = os.path.join(
-                        EXTRACT_WORKING_FOLDER, folder_name)
+                        EXTRACT_WORKING_FOLDER, folder_name
+                    )
                     item_path = os.path.join(subfolder_path, file_name)
-                    with open(item_path, 'wb') as dest_file:
+                    with open(item_path, "wb") as dest_file:
                         dest_file.write(zipfile.read(zip_entry))
 
     def extract_zips(self):
         """(Re-)extract the downloaded SNOMED distribution zipfiles"""
         # Extract zips afresh each time there is a change
-        LOGGER.debug('Starting SNOMED zip extraction')
+        LOGGER.debug("Starting SNOMED zip extraction")
         current_files = os.listdir(EXTRACT_WORKING_FOLDER)
         for current_file in current_files:
-
             current_path = os.path.join(EXTRACT_WORKING_FOLDER, current_file)
             if not os.path.isdir(current_path):
                 os.remove(current_path)
             else:
                 shutil.rmtree(current_path, ignore_errors=False)
 
-        source_file_paths = self.get_cached_file_paths()
+        source_file_paths = [
+            file_path for file_path in self.get_cached_file_paths()
+        ]
 
         for source_file_path in source_file_paths:
-            if source_file_path.endswith('.zip'):
+            if source_file_path.endswith(".zip"):
                 zf = zipfile.ZipFile(
-                    WORKING_FOLDER +
-                    source_file_path.replace('downloads/', ''), 'r')
+                    WORKING_FOLDER
+                    + source_file_path.replace("downloads/", ""),
+                    "r",
+                )
                 entries = zf.namelist()
                 for entry in entries:
                     self.save_zip_entry(zf, entry)
-        LOGGER.debug('Finished SNOMED zip extraction')
+        LOGGER.debug("Finished SNOMED zip extraction")
 
 
 @click.group()
@@ -261,14 +280,16 @@ def snomed_data(context):
 def fetch(dropbox_client):
     """Downloads content from dropbox and extracts it to specific folders"""
     if not dropbox_client.has_internet_connection:
-        LOGGER.warning('No internet connection')
+        LOGGER.warning("No internet connection")
         return
     # First, synchronize with Dropbox
     try:
         if dropbox_client.metadata_has_changed():
             # Save the new metadata
-            LOGGER.info('Metadata has changed, saving the current state')
-            dropbox_client.save_metadata(dropbox_client.path_keyed_upstream_metadata)
+            LOGGER.info("Metadata has changed, saving the current state")
+            dropbox_client.save_metadata(
+                dropbox_client.path_keyed_upstream_metadata
+            )
 
             # SANITY check; there should only be two files in the WORKING_FOLDER
             # folder, until some future date when we start authoring
@@ -279,54 +300,59 @@ def fetch(dropbox_client):
 
             # 'uk_sct2clfull' should match 1 path
             # 'uk_sct2drfull' should match 1 path
-            clinical_path = re.compile(r'^.*uk_sct2clfull.+')
-            drug_path = re.compile(r'^.*uk_sct2drfull.+')
+            clinical_path = re.compile(r"^.*uk_sct2cl.+")
+            drug_path = re.compile(r"^.*uk_sct2dr.+")
 
-            cached_drug_paths = [drug_path.match(file_path)
-                                  for file_path
-                                  in cached_file_paths
-                                  if drug_path.match(file_path) is not None]
+            cached_drug_paths = [
+                drug_path.match(file_path)
+                for file_path in cached_file_paths
+                if drug_path.match(file_path) is not None
+            ]
 
             assert len(cached_drug_paths) == 1
 
-            cached_clinical_paths = [clinical_path.match(file_path)
-                                     for file_path
-                                     in cached_file_paths
-                                     if clinical_path.match(file_path) is not None]
+            cached_clinical_paths = [
+                clinical_path.match(file_path)
+                for file_path in cached_file_paths
+                if clinical_path.match(file_path) is not None
+            ]
 
             assert len(cached_clinical_paths) == 1
 
-            LOGGER.info("Correct number of release packages found. Continuing...")
+            LOGGER.info(
+                "Correct number of release packages found. Continuing..."
+            )
 
             # Check which files have changed upstream and queue to download
             for upstream_file_path in dropbox_client.get_upstream_file_paths():
                 if dropbox_client.file_has_changed(upstream_file_path):
-                    LOGGER.info('Fetching %s' % upstream_file_path)
+                    LOGGER.info("Fetching %s" % upstream_file_path)
                     dropbox_client.fetch_file(upstream_file_path)
 
             dropbox_client.extract_zips()
         else:
             LOGGER.info(
-                'There is no change in the Dropbox folder. '
-                'Happy to do nothing! However, '
+                "There is no change in the Dropbox folder. "
+                "Happy to do nothing! However, "
                 'if your "%s" or "%s" folders are inconsistent, delete '
-                'them and re-run this command' %
-                (WORKING_FOLDER, EXTRACT_WORKING_FOLDER)
+                "them and re-run this command"
+                % (WORKING_FOLDER, EXTRACT_WORKING_FOLDER)
             )
     except MaxRetryError as e:
         LOGGER.warning(
             'UNABLE to reach Dropbox servers: "%s".'
-            'We will proceed with the build in the hope that the content '
-            'has not changed since the last sync. That hope might be '
-            'unwarranted.'
-            % e)
+            "We will proceed with the build in the hope that the content "
+            "has not changed since the last sync. That hope might be "
+            "unwarranted." % e
+        )
 
 
 @snomed_data.command()
 def clear():
     """Very unsafe; to be run only to clear space on CircleCI"""
     shutil.rmtree(WORKING_FOLDER, ignore_errors=False)
-    shutil.rmtree(EXTRACT_WORKING_FOLDER,ignore_errors=False)
+    shutil.rmtree(EXTRACT_WORKING_FOLDER, ignore_errors=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     snomed_data()
